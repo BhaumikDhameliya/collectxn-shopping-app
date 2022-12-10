4d990d604a5613b45f275083581a26f0627f7f28
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2022-12-10 23:04:10 +0530

    category in footer and redirect to that category page

--- src/components/Footer/QuickLinks.jsx
+++ src/components/Footer/QuickLinks.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
 
 const QuickLinks = () => {
+  const navigate = useNavigate()
+  const categories = useSelector((state) => state.category.categories)
+
   return (
     <div className="flex w-full gap-12 tablet:justify-around font-medium text-base tablet:text-xl">
       <div className="flex flex-col gap-4">
         <div className="font-cera-pro font-bold text-xs tablet:text-lg text-[#A8A8A8]">
           Quick links
         </div>
-        <div>
-          <Link to="/">Sneakers</Link>
-        </div>
-        <div>
-          <Link to="/">Apparels</Link>
-        </div>
-        <div>
-          <Link to="/">Jewellary</Link>
-        </div>
-        <div>
-          <Link to="/">Accessories</Link>
-        </div>
-        <div>
-          <Link to="/">Playstation</Link>
-        </div>
+        {Object.entries(categories || {})?.map(([categoryId, category]) => {
+          return (
+            <div
+              onClick={() => {
+                navigate(`/category/${categoryId}`)
+              }}
+            >
+              <span>{category?.name}</span>
+            </div>
+          )
+        })}
       </div>
       <div className="flex flex-col gap-4">
         <div className="font-cera-pro font-bold text-xs tablet:text-lg text-[#A8A8A8]">
