c11b3d5a15ec9540eb10b2613a3ced5b2e61b453
DeepBoda <deepboda18@gmail.com>
2022-12-21 23:38:22 +0530

    temp baseurl/build

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'https://backend.collectxn.store/api/v1'
+// export const BASE_URL = 'https://backend.collectxn.store/api/v1'
+export const BASE_URL = 'http://3.109.34.101:3000/api/v1'
 export const RE_DIGIT = new RegExp(/^\d+$/)
