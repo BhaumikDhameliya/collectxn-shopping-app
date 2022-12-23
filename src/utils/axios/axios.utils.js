d6bae76a08ca66cea64a44b82fb942ca3b8ad0d9
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2022-12-23 22:32:25 +0530

    added response interceptor to redirect to login page if required

--- src/utils/axios/axios.utils.js
+++ src/utils/axios/axios.utils.js
@@ -4,3 +4,16 @@ import { BASE_URL } from '../../config/constants'
 export const collectionAPI = axios.create({
   baseURL: BASE_URL,
 })
+
+collectionAPI.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error) {
+    const errorRes = error?.response?.data?.message
+    if (errorRes && errorRes === 'JWT Token is required') {
+      window.location.replace('/auth/login')
+    }
+    return Promise.reject(error)
+  },
+)
